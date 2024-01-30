925242b0e8806be27bc1db1f5778c4bbed71fad9
Ram Krishnan <ram.krishnan@hudini.io>
2024-01-30 15:09:47 +0000

    Merged in feature/pw-317-Open-Redirect-URLs-in-a-drawer (pull request #145)
    Feature/pw 317 Open Redirect URLs in a drawer
    
    * chore:console.log remove
    
    * feat:opening link in new tab using anchor tag approach
    
    * feat: iframe navbar color updated
    
    * feat: iframe title prop is added
    
    * chore: file name change
    
    * chore: resolving merge conflit
    
    * feat: updated iframe title
    
    * refactor:fixed typescript error while build the app and replaced string with constant approach
    
    * fix:iframe component is being shevled and going back to previously existed logic to load external url
    
    * Merge branch 'master' into feature/pw-317-Open-Redirect-URLs-in-a-drawer
    
    
    Approved-by: Somanatha S Biradar

--- components/shared/IframeComponent/IframeComponent.tsx
+++ components/shared/IframeComponent/IframeComponent.tsx
@@ -1,8 +1,4 @@
-import styles from './IframeComponent.module.scss';
-import cx from 'classnames';
-import { useRef, useEffect, useState } from 'react';
 import { openLinknewTab } from 'utils/functions';
-import CloseButton from '@icons/closeButton.svg';
 
 interface IFrameProps {
   src: string;
@@ -11,41 +7,8 @@ interface IFrameProps {
 }
 
 export const IframeComponent: React.FC<IFrameProps> = ({ src, handledrawerState, name }) => {
-  const iframeRef = useRef<HTMLIFrameElement>(null);
-  const [loaded, setLoaded] = useState(false);
-  const { hostname } = new URL(src);
+  handledrawerState(false);
+  openLinknewTab(src);
 
-  useEffect(() => {
-    if (loaded) {
-      const iframe = iframeRef.current && iframeRef.current.contentWindow;
-      !iframe?.length && (handledrawerState(false), openLinknewTab(src));
-    }
-  }, [loaded, src, handledrawerState]);
-
-  return (
-    <div className={cx(styles.iframeWrapper)}>
-      <div className={cx(styles.navbar)}>
-        <button
-          type='button'
-          onClick={() => {
-            handledrawerState(false);
-          }}
-          className={cx(styles.closeButton)}
-        >
-          <CloseButton />
-        </button>
-        <div className={cx(styles.linkDisplay)}>
-          <p className={cx(styles.title)}>{name}</p>
-          <p className={cx(styles.hostName)}>{hostname}</p>
-        </div>
-      </div>
-      <iframe
-        src={src}
-        frameBorder='0'
-        className={cx(styles.iframe)}
-        onLoad={() => setLoaded(true)}
-        ref={iframeRef}
-      ></iframe>
-    </div>
-  );
+  return null;
 };
