26aad85e063ce9dc51e425cd20cff2c254e0faa8
Somanatha S Biradar <somanatha.biradar@hudini.io>
2024-02-02 07:34:31 +0000

    Merged in feature/pw-384-meta-image (pull request #156)
    fix: update path
    
    * fix: update path
    
    
    Approved-by: rameez

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -38,7 +38,7 @@ class MyDocument extends Document {
           <meta key='robots' name='robots' content='noindex,nofollow' />
           <meta key='googlebot' name='googlebot' content='noindex,nofollow' />
           <meta name='description' content='Feel the immersive digital experience with us' />
-          <meta name='image' content={`/${BRAND_CODE}.ico`} />
+          <meta name='image' content={`/images/${BRAND_CODE}/Logo.png`} />
           <meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1' />
         </Head>
         <body>
