71f5ef80d68f1bd05c3e33e903960b0448292503
rameez <rameez.kalathil@hudini.io>
2024-03-18 09:39:53 +0000

    Merged in hotfix/PW-456-Remove-placeholder-from-dining-detail-drawer (pull request #194)
    removed placeholder
    
    * removed placeholder
    
    * icon fix
    
    
    Approved-by: Somanatha S Biradar

--- components/pages/dining/DiningDetailsDrawer/DiningDetailsDrawer.tsx
+++ components/pages/dining/DiningDetailsDrawer/DiningDetailsDrawer.tsx
@@ -366,13 +366,11 @@ const DiningDetailsDrawer = () => {
     return (
       <>
         <>
-          {selectedItem?.images?.length > 0 ? (
+          {selectedItem?.images?.length > 0 && (
             <StableImage
               className={styles.image}
               src={`${ASSETS_URL}/${selectedItem?.images[0]?.ratio16to9}`}
             />
-          ) : (
-            <PlaceholderImage />
           )}
           {selectedItem?.name && (
             <div className={styles.titleWrapper}>
