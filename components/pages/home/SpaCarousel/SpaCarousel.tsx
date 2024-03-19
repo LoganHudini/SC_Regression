e055f5465a5899a14d15f5f1835ec8f5d75d3242
rameez <rameez.kalathil@hudini.io>
2024-03-19 16:59:13 +0000

    Merged in hotfix/PW-456-Remove-placeholder-from-dining-detail-drawer (pull request #195)
    Hotfix/PW-456 Remove placeholder from dining detail drawer
    
    * removed placeholder
    
    * icon fix
    
    * alignment issue solved
    
    * icon fix
    
    * commit
    
    * icons alignment
    
    * fix: icon sizes
    
    * spacarousal CTA
    
    
    Approved-by: Somanatha S Biradar

--- components/pages/home/SpaCarousel/SpaCarousel.tsx
+++ components/pages/home/SpaCarousel/SpaCarousel.tsx
@@ -52,10 +52,12 @@ export const CarouselSlide: React.FC<ICarouselSlideProps> = ({ slide, slideStyle
         })}
         onClick={handleClick}
       >
-        <StableImage
-          className={cx(styles.carouselSlideImage, 'globals-carouselSlideImage')}
-          src={`${ASSETS_URL}/${slide?.images[0]?.master}`}
-        />
+        {slide?.images?.length > 0 && (
+          <StableImage
+            className={cx(styles.carouselSlideImage, 'globals-carouselSlideImage')}
+            src={`${ASSETS_URL}/${slide?.images[0]?.master}`}
+          />
+        )}
         <div
           className={cx(
             styles.carouselSlideDetailsWrapper,
@@ -140,29 +142,17 @@ export const SpaCarousel: React.FC<ICarouselProps> = ({ data }) => {
   const time = getTimings(spaInfoDetails?.customAttributes);
 
   const spaDetails = () => (
-    <>
-      {spaInfoDetails?.images?.length > 0 ? (
+    <div
+      className={cx({
+        [styles.listComponentMargin]:
+          spaTreatments?.length > 0 || spaInfoDetails?.cta?.status === ACTIVE,
+      })}
+    >
+      {spaInfoDetails?.images?.length > 0 && (
         <StableImage
           className={styles.image}
           src={`${ASSETS_URL}/${spaInfoDetails?.images[0]?.ratio16to9}`}
         />
-      ) : (
-        <PlaceholderImage />
-      )}
-
-      {(spaTreatments?.length > 0 || spaInfoDetails?.cta?.status === ACTIVE) && (
-        <StyledButton
-          variant='contained'
-          onClick={onCtaClick}
-          className={cx(styles.button, {
-            [styles.withoutImageButton]: spaInfoDetails && !spaInfoDetails?.images[0]?.ratio16to9,
-          })}
-        >
-          {spaTreatments?.length > 0
-            ? t('View Treatments')
-            : spaInfoDetails?.cta?.status === ACTIVE &&
-              (spaInfoDetails?.cta?.ctaTitle || t('BOOK NOW'))}
-        </StyledButton>
       )}
 
       <div className={styles.wrapper}>
@@ -219,7 +209,15 @@ export const SpaCarousel: React.FC<ICarouselProps> = ({ data }) => {
           )}
         </div>
       </div>
-    </>
+      {(spaTreatments?.length > 0 || spaInfoDetails?.cta?.status === ACTIVE) && (
+        <StyledButton variant='contained' onClick={onCtaClick} className={cx(styles.button)}>
+          {spaTreatments?.length > 0
+            ? t('View Treatments')
+            : spaInfoDetails?.cta?.status === ACTIVE &&
+              (spaInfoDetails?.cta?.ctaTitle || t('BOOK NOW'))}
+        </StyledButton>
+      )}
+    </div>
   );
   return (
     <>
