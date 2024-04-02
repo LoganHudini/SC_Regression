43b666fafdf14885b57a118a2f37e234c03f5ce5
rameez <rameez.kalathil@hudini.io>
2024-04-02 12:38:31 +0000

    Merged in hotfix/RAF-43-Hotfix-IRD]--Dropdown-is-not-showing-as-In-room-dining-when-navigating-from-home-page-if-already-once-the-drop-down-selection-changed-to-restaurant (pull request #207)
    category dropdown issue fixed in IRD
    
    * category dropdown issue fixed in IRD
    
    
    Approved-by: Somanatha S Biradar

--- pages/[locale]/[hotel]/dining.tsx
+++ pages/[locale]/[hotel]/dining.tsx
@@ -95,6 +95,7 @@ const Dining = () => {
   );
 
   useEffect(() => {
+    diningOptions({ type: IN_ROOM_DINING });
     if (
       uniqueFilteredDiningOptions?.length > 0 &&
       irdOption?.length === 0 &&
@@ -105,7 +106,6 @@ const Dining = () => {
           ? [...uniqueFilteredDiningOptions, { type: IN_ROOM_DINING }]
           : uniqueFilteredDiningOptions,
       );
-      diningOptions({ type: IN_ROOM_DINING });
     }
   }, [queryResultsData]);
 
