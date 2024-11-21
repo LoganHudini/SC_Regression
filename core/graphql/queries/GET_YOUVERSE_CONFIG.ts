ccc3f540ff4a4eba8510267561c4045cc87e4284
Somanatha S Biradar <somanatha.biradar@hudini.io>
2024-11-21 08:43:50 +0000

    Merged in bugfix/rjp-28 (pull request #550)
    Bugfix/rjp 28
    
    * fix: accompany guest validation
    
    * fix: youverse issue
    
    
    Approved-by: rameez

--- core/graphql/queries/GET_YOUVERSE_CONFIG.ts
+++ core/graphql/queries/GET_YOUVERSE_CONFIG.ts
@@ -1,12 +1,12 @@
 import { gql } from '@apollo/client';
-// import { HOTEL_ID } from '../endpoints';
+import { HOTEL_ID } from '../endpoints';
 
 export const GET_YOUVERSE_CONFIG = gql`
   query GetYoonikConfig($body: any, $confirmationId: string) {
     getyoonikconfig(body: $body, confirmationId: $confirmationId)
       @rest(
         type: "GetYoonikConfigPayload"
-        path: "/initiatekyc/hotel/53c7081b-ecb9-4492-a5fa-8e3c55639967?confirmationId={args.confirmationId}"
+        path: "/initiatekyc/hotel/${HOTEL_ID}?confirmationId={args.confirmationId}"
         method: "POST"
         bodyKey: "body"
       ) {
