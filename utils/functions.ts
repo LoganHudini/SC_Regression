d5f8ca876e34e10a170f165e62e12417fce3ae2a
rameez <rameez.kalathil@hudini.io>
2025-04-21 11:54:06 +0000

    Merged in hotfix/pw-91 (pull request #749)
    pair to room activated
    
    * pair to room activated
    
    * fix: build error
    
    
    Approved-by: Somanatha S Biradar

--- utils/functions.ts
+++ utils/functions.ts
@@ -73,7 +73,7 @@ export const getWelcomeDrawer = () =>
 // Convert time format from 24H to 12H
 export const convertTo12HourFormat = (time24: string) => {
   if (!time24) return time24 || '';
-  const [hours, minutes] = time24?.split(':');
+  const [hours, minutes] = time24 && time24.split(':');
 
   let hoursNum = parseInt(hours, 10);
   const meridiem = hoursNum >= 12 ? 'PM' : 'AM';
