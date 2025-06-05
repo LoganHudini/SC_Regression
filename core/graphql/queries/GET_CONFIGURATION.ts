5e37d3a43f2fcb917a5cd030b3b1ac4e0c245f56
Shreyas <shreyas.s@hudini.io>
2025-06-05 12:04:33 +0530

    adding trential to auth stage

--- core/graphql/queries/GET_CONFIGURATION.ts
+++ core/graphql/queries/GET_CONFIGURATION.ts
@@ -1177,7 +1177,7 @@ export const configuration: any = [
               {
                 name: 'Guest Information',
                 isActive: true,
-                type: 'manual',
+                type: 'trential',
                 allowSkipBiometrics: true,
                 details: [
                   {
