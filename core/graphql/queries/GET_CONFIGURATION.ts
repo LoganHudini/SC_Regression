0d3e0d53fc612b1410135084ee7bb2b6dd02df5d
Athira S L <athira.sl@hudini.io>
2025-07-29 18:25:11 +0530

    Updated

--- core/graphql/queries/GET_CONFIGURATION.ts
+++ core/graphql/queries/GET_CONFIGURATION.ts
@@ -24943,7 +24943,7 @@ export const configuration: any = [
       {
         code: 'In-Room Dining',
         name: 'In-Room Dining',
-        isActive: false,
+        isActive: true,
         signatureRequired: true,
         type: 'VENDOR',
         payment: [{ id: '1', name: 'BILL TO ROOM', message: '' }],
