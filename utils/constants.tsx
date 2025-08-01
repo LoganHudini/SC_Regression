518341aa668503391e1527b5eb1a6636b5fbc6fa
Athira S L <athira.sl@hudini.io>
2025-08-01 06:30:59 +0000

    Merged in config/DEP-40-fairmont-carton-house-configuration (pull request #991)
    Config/DEP-40 fairmont carton house configuration
    
    * Added fairmont-carton-house to the configuration
    
    * update
    
    
    Approved-by: Sahal Sain

--- utils/constants.tsx
+++ utils/constants.tsx
@@ -45,6 +45,8 @@ export const FAIRMONT_CENTURY_PLAZA = 'fairmont-century-plaza';
 export const FAIRMONT_UDAIPUR = 'fairmont-udaipur';
 export const FAIRMONT_RAMLA = 'fairmont-ramla';
 export const FAIRMONT_MACDONALD = 'fairmont-macdonald';
+export const FAIRMONT_CARTON_HOUSE = 'fairmont-carton-house';
+
 export const ONE_HOTELS_SOUTH_BEACH = '1-hotel-south-beach';
 export const HARD_ROCK_HOTEL_CANCUN = 'hard-rock-hotel-cancun';
 export const ATLANTIS = 'atlantis';
