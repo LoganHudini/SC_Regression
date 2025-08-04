112a98169e4fd64a4db7db1bb2ee7b9ffd063b3e
Athira S L <athira.sl@hudini.io>
2025-08-04 06:00:33 +0000

    Merged in config/DEP-63-fairmont-orchid-configuration (pull request #997)
    added fairmont-orchid to the configuration
    
    * added fairmont-orchid to the configuration
    
    
    Approved-by: Sahal Sain

--- utils/constants.tsx
+++ utils/constants.tsx
@@ -46,6 +46,7 @@ export const FAIRMONT_UDAIPUR = 'fairmont-udaipur';
 export const FAIRMONT_RAMLA = 'fairmont-ramla';
 export const FAIRMONT_MACDONALD = 'fairmont-macdonald';
 export const FAIRMONT_CARTON_HOUSE = 'fairmont-carton-house';
+export const FAIRMONT_ORCHID = 'fairmont-orchid';
 
 export const ONE_HOTELS_SOUTH_BEACH = '1-hotel-south-beach';
 export const HARD_ROCK_HOTEL_CANCUN = 'hard-rock-hotel-cancun';
