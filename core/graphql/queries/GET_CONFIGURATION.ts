b499f3ff410d89ae8845b5e0dbe2a3f8e808fef4
Rameez Ahammed K <rameez.kalathil@hudini.io>
2025-08-19 17:13:32 +0530

    reverted

--- core/graphql/queries/GET_CONFIGURATION.ts
+++ core/graphql/queries/GET_CONFIGURATION.ts
@@ -4950,7 +4950,7 @@ export const configuration: any = [
       {
         code: 'In-Room Dining',
         name: 'In-Room Dining',
-        isActive: true,
+        isActive: false,
         type: 'CMS',
         payment: [{ id: '1', name: 'BILL TO ROOM', message: '' }],
       },
