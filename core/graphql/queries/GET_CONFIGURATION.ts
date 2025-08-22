b9c4b832cd9056b2ad176bffc33a14bd523d0831
Sayoojya PM <sayoojya.pm@hudini.io>
2025-08-22 08:25:25 +0000

    Merged in bugFix/hp-364-raffles-gender-not-prefilling (pull request #1070)
    fix: gender
    
    * fix: gender
    
    * Merged master into bugFix/hp-364-raffles-gender-not-prefilling
    
    
    Approved-by: Sahal Sain

--- core/graphql/queries/GET_CONFIGURATION.ts
+++ core/graphql/queries/GET_CONFIGURATION.ts
@@ -27674,12 +27674,12 @@ export const configuration: any = [
                     options: [
                       {
                         name: 'Male',
-                        value: 'MALE',
+                        value: 'M',
                         vendorGenderType: 'M',
                       },
                       {
                         name: 'Female',
-                        value: 'FEMALE',
+                        value: 'F',
                         vendorGenderType: 'F',
                       },
                       {
