7b4b08003c39e31187ccc9723943997211869778
Shreyas S. <shreyas.s@hudini.io>
2025-08-22 08:25:44 +0000

    Merged in config/DEP-25-IRD (pull request #1075)
    DEP-25
    
    * DEP-25
    
    
    Approved-by: Sahal Sain

--- core/graphql/queries/GET_CONFIGURATION.ts
+++ core/graphql/queries/GET_CONFIGURATION.ts
@@ -11636,7 +11636,7 @@ export const configuration: any = [
         code: 'In-Room Dining',
         name: 'In-Room Dining',
         isActive: true,
-        type: 'CMS',
+        type: 'VENDOR',
         payment: [{ id: '1', name: 'BILL TO ROOM', message: '' }],
       },
       {
